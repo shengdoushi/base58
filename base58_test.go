dbe4df27350e30cd68fa4b1157191aa6909fb3a8
guolihui <guolihui112@gmail.com>
2018-05-22 16:58:54 +0800

    gofmt

--- base58_test.go
+++ base58_test.go
@@ -1,15 +1,15 @@
 package base58
 
 import (
-	"testing"
 	"bytes"
-	"math"
 	"fmt"
-	"time"
+	"math"
 	"math/rand"
+	"testing"
+	"time"
 )
 
-func TestAlphabetImplStringer(t *testing.T){
+func TestAlphabetImplStringer(t *testing.T) {
 	// interface: Stringer {String()}
 	alphabetStr := "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 	alphabet := NewAlphabet(alphabetStr)
@@ -18,11 +18,11 @@ func TestAlphabetImplStringer(t *testing.T){
 	}
 }
 
-func TestAlphabetFix58Length(t *testing.T){
+func TestAlphabetFix58Length(t *testing.T) {
 	crashed := false
 	alphabetStr := "sfdjskdf"
 
-	defer func(){
+	defer func() {
 		if err := recover(); err != nil {
 			crashed = true
 		}
@@ -34,18 +34,18 @@ func TestAlphabetFix58Length(t *testing.T){
 	NewAlphabet(alphabetStr)
 }
 
-func TestUnicodeAlphabet(t *testing.T){
+func TestUnicodeAlphabet(t *testing.T) {
 	myAlphabet := NewAlphabet("一二三四五六七八九十壹贰叁肆伍陆柒捌玖零拾佰仟万亿圆甲乙丙丁戊己庚辛壬癸子丑寅卯辰巳午未申酉戌亥金木水火土雷电风雨福")
 
 	testCases := []struct {
-		input []byte
+		input  []byte
 		should string
 	}{
 		{[]byte{0}, "一"},
-		{[]byte{0,0}, "一一"},
+		{[]byte{0, 0}, "一一"},
 		{[]byte{1}, "二"},
-		{[]byte{0,1}, "一二"},
-		{[]byte{1,1}, "五圆"},
+		{[]byte{0, 1}, "一二"},
+		{[]byte{1, 1}, "五圆"},
 	}
 
 	for _, testItem := range testCases {
@@ -57,15 +57,15 @@ func TestUnicodeAlphabet(t *testing.T){
 		resultInput, err := Decode(testItem.should, myAlphabet)
 		if err != nil {
 			t.Errorf("decodeBase58(%s) error : %s", testItem.should, err.Error())
-		}else if !bytes.Equal(resultInput, testItem.input){
+		} else if !bytes.Equal(resultInput, testItem.input) {
 			t.Errorf("decodeBase58(%s) should %v, but %v", testItem.should, testItem.input, resultInput)
 		}
 	}
 }
 
-func TestRandCases(t *testing.T){
+func TestRandCases(t *testing.T) {
 	randSeed := time.Now().UnixNano()
-    r := rand.New(rand.NewSource(randSeed))
+	r := rand.New(rand.NewSource(randSeed))
 
 	// generate 256 bytes
 	testBytes := make([]byte, r.Intn(1000))
@@ -83,7 +83,7 @@ func TestRandCases(t *testing.T){
 	logTag := fmt.Sprintf("rand[%d]", randSeed)
 	// Encode
 	calc58Str := Encode(testBytes, alphabet)
-	if calc58Str != string(should58Runes){
+	if calc58Str != string(should58Runes) {
 		t.Errorf("%s encodeBase58(%v) should %s, but %s", logTag, testBytes, string(should58Runes), calc58Str)
 	}
 
@@ -91,14 +91,14 @@ func TestRandCases(t *testing.T){
 	decodeBytes, err := Decode(string(should58Runes), alphabet)
 	if err != nil {
 		t.Errorf("%s decodeBase58(%s) error : %s", logTag, string(should58Runes), err.Error())
-	}else if !bytes.Equal(decodeBytes, testBytes){
+	} else if !bytes.Equal(decodeBytes, testBytes) {
 		t.Errorf("%s decodeBase58(%s) should %v, but %v", logTag, string(should58Runes), testBytes, decodeBytes)
 	}
 }
 
-
 // test: [0]: input bytes  [1]: encoded string
 var testCases [][][]byte
+
 func init() {
 	caseCount := 1000000
 	testCases = make([][][]byte, caseCount)
@@ -123,12 +123,12 @@ func BenchmarkDecode(b *testing.B) {
 
 ////////////////////////////////////////////////////////////////////////////////
 // redix trans
-func redixTrans256and58(input []byte, fromRedix uint32, toRedix uint32)([]byte, error){
-	capacity := int(math.Log(float64(fromRedix)) / math.Log(float64(toRedix))) + 1
+func redixTrans256and58(input []byte, fromRedix uint32, toRedix uint32) ([]byte, error) {
+	capacity := int(math.Log(float64(fromRedix))/math.Log(float64(toRedix))) + 1
 
 	zeros := 0
 	for zeros < len(input) && input[zeros] == 0 {
-		zeros ++
+		zeros++
 	}
 
 	output := make([]byte, 0, capacity)
@@ -144,48 +144,48 @@ func redixTrans256and58(input []byte, fromRedix uint32, toRedix uint32)([]byte, 
 		}
 
 		for carry != 0 {
-			output = append(output, byte(carry % toRedix))
+			output = append(output, byte(carry%toRedix))
 			carry /= toRedix
 		}
-		
+
 	}
 
-	for i := 0; i < zeros; i++{
+	for i := 0; i < zeros; i++ {
 		output = append(output, 0)
 	}
 
 	// reverse
-	for idx := 0; idx < len(output)/2; idx++{
+	for idx := 0; idx < len(output)/2; idx++ {
 		output[len(output)-idx-1], output[idx] = output[idx], output[len(output)-idx-1]
 	}
 	return output, nil
 }
 
-func TestTransRedix(t *testing.T){
-	for _, testItem := range []struct{
+func TestTransRedix(t *testing.T) {
+	for _, testItem := range []struct {
 		num256 []byte
-		num58 []byte
+		num58  []byte
 	}{
 		{[]byte{0}, []byte{0}},
-		{[]byte{0,0}, []byte{0, 0}},
-		{[]byte{0,0,0}, []byte{0, 0, 0}},
+		{[]byte{0, 0}, []byte{0, 0}},
+		{[]byte{0, 0, 0}, []byte{0, 0, 0}},
 		{[]byte{1}, []byte{1}},
 		{[]byte{57}, []byte{57}},
-		{[]byte{58}, []byte{1,0}},
-		{[]byte{1,0}, []byte{4,24}},
-		{[]byte{14,239}, []byte{1,7,53}},
-	}{
+		{[]byte{58}, []byte{1, 0}},
+		{[]byte{1, 0}, []byte{4, 24}},
+		{[]byte{14, 239}, []byte{1, 7, 53}},
+	} {
 		calc58, err := redixTrans256and58(testItem.num256, 256, 58)
 		if err != nil {
 			t.Errorf("redix256to58: %v should be %v, but error %s", testItem.num256, testItem.num58, err.Error())
-		}else if !bytes.Equal(calc58, testItem.num58){
+		} else if !bytes.Equal(calc58, testItem.num58) {
 			t.Errorf("redix256to58: %v should be %v, but %v", testItem.num256, testItem.num58, calc58)
 		}
 
 		calc256, err := redixTrans256and58(testItem.num58, 58, 256)
 		if err != nil {
 			t.Errorf("redix58to256: %v should be %v, but error %s", testItem.num58, testItem.num256, err.Error())
-		}else if !bytes.Equal(calc256, testItem.num256){
+		} else if !bytes.Equal(calc256, testItem.num256) {
 			t.Errorf("redix58to256: %v should be %v, but %v", testItem.num58, testItem.num256, calc256)
 		}
 	}
--- example_test.go
+++ example_test.go
@@ -8,22 +8,22 @@ import (
 
 // This Example shows the basic usage of the package:
 // Encode, Decode
-func Example_basic(){
+func Example_basic() {
 	// use bitcoin alphabet, just same as: base58.NewAlphabet("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
 	myAlphabet := base58.BitcoinAlphabet
-	
+
 	// encode
-	input := []byte{0,0,0,1,2,3}
+	input := []byte{0, 0, 0, 1, 2, 3}
 	encodedString := base58.Encode(input, myAlphabet)
 	fmt.Printf("base58encode(%v) = %s\n", input, encodedString)
-	
+
 	// decode
 	decodedBytes, err := base58.Decode(encodedString, myAlphabet)
 	if err != nil { // error occurred when encodedString contains character not in alphabet
 		log.Fatal("base58Decode error:", err)
-	}else{
+	} else {
 		fmt.Printf("base58decode(%s) = %v\n", encodedString, decodedBytes)
-	}	
+	}
 
 	// Output:
 	// base58encode([0 0 0 1 2 3]) = 111Ldp
